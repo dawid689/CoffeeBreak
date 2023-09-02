b3a9a0d9947f4440d8b45c679a9b0cb7c3d9c8bc
Dawid <dsienkiewicz689@gmail.com>
2023-09-02 23:16:10 +0200

    User registration

---
+++ Java Backend/com.cafe/src/main/java/com/cafe/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.cafe.dao;
+
+import com.cafe.POJO.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+
+    User findByEmailId(@Param("email") String email);
+
+}
